eb4208e3354da971e939a55c258c45d59dd7bbb1
alexfont <alexfont321@gmail.com>
2018-10-10 10:45:45 -0500

    finished try and catch ex

---
+++ AddressBook.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace tryCatch 
+{
+    public class AddressBook 
+    {
+        Dictionary<string, Contact> addressBook = new Dictionary<string, Contact>();
+
+        public void AddContact(Contact contact) {
+            try 
+            {
+            addressBook.Add(contact.Email, contact);
+            }
+            catch (ArgumentException ex) {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        public Contact GetByEmail(string email) {
+           
+            return addressBook[email];
+            
+        }
+    }
+}
